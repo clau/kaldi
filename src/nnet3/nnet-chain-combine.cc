57dde7aaf7c3e92c42341602bc74cc2c9705c2a8
Chi Wai Lau <clau@apple.com>
2017-12-19 15:08:11 -0800

    Test

--- src/nnet3/nnet-chain-combine.cc
+++ src/nnet3/nnet-chain-combine.cc
@@ -175,10 +175,10 @@ void NnetChainCombiner::Combine() {
     bool reset_fisher_memory = false;
     if (adaqn.ShouldResetFisherMemory()) {
       Vector<double> df(dim);
-      const VectorBase<double> &x_o = adaqn.GetLastCheckpointedValue();
-      const VectorBase<double> &x_n = adaqn.GetAverageValueSinceCheckpoint();
-      double f_o = ComputeObjfAndDerivFromParameters(*x_o, &df);
-      double f_n = ComputeObjfAndDerivFromParameters(*x_n, &df);
+      Vector<double> &x_o = adaqn.GetLastCheckpointedValue();
+      Vector<double> &x_n = adaqn.GetAverageValueSinceCheckpoint();
+      double f_o = ComputeObjfAndDerivFromParameters(x_o, &df);
+      double f_n = ComputeObjfAndDerivFromParameters(x_n, &df);
       reset_fisher_memory = f_n > 1.01 * f_o;
     }
     adaqn.DoStep(objf, deriv, reset_fisher_memory);
